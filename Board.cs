037140821a6c409d1d6ffa5797dd0d963085366f
coolxer <lukasik4789@gmail.com>
2019-03-09 22:57:35 +0100

    added basics classes

---
+++ Board.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess
+{
+    class Board
+    {
+        private int width = 8;
+        private int height = 8;
+
+        private char[,] poles;
+
+        public Board()
+        {
+           
+        }
+
+        public void draw()
+        {
+            Console.WriteLine("----------");
+
+            
+
+            Console.WriteLine("----------");
+        }
+
+    }
+}
---
+++ Figure.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess
+{
+    class Figure
+    {
+        private struct pos
+        {
+            short x;
+            short y;
+        };
+
+        private char value;
+        private char color;
+
+        private int[] moves; //top top-right right bottom-right bottom bottom-left left top-left
+
+
+        public void move()
+        {
+
+        }
+
+        public void allowMoves()
+        {
+
+        }        
+    }
+}
