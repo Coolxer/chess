c804f9ec6a109b01c75ce9e30d291e1ef5a3547e
coolxer <lukasik4789@gmail.com>
2019-03-18 14:45:38 +0100

    rook movements repaired

--- Rook.cs
+++ Rook.cs
@@ -19,23 +19,54 @@ namespace chess
 
             //top
             if (pos.X > 0)
+            {
                 for (int i = pos.X - 1; i >= 0; i--)
+                {
+                    if (board.fields[i, pos.Y] != null && board.fields[i, pos.Y].color == color)
+                        break;
+
                     matrix[i, pos.Y] = true;
+                }   
+            }
+                
 
             //right
-            if(pos.Y < 8)
-                for (int i = pos.Y + 1; i < 8; i++)
+            if(pos.Y < 7)
+            {
+                for (int i = pos.Y + 1; i <= 7; i++)
+                {
+                    if (board.fields[pos.X, i] != null && board.fields[pos.X, i].color == color)
+                        break;
+
                     matrix[pos.X, i] = true;
+                }     
+            }
+                
 
             //down
-            if(pos.X < 8)
-                for (int i = pos.X + 1; i < 8; i++)
+            if(pos.X < 7)
+            {
+                for (int i = pos.X + 1; i <= 7; i++)
+                {
+                    if (board.fields[i, pos.Y] != null && board.fields[i, pos.Y].color == color)
+                        break;
+
                     matrix[i, pos.Y] = true;
+                }         
+            }
+                
 
             //left
             if(pos.X > 0)
+            {
                 for (int i = pos.X - 1; i >= 0; i--)
+                {
+                    if (board.fields[pos.X, i] != null && board.fields[pos.X, i].color == color)
+                        break;
+
                     matrix[pos.X, i] = true;
+                }
+            }
 
             for (int i = 0; i < 8; i++)
             {
